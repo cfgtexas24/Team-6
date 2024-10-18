5140a68ba6613c989096b245eea363e0983a0b91
md-y <15069105+md-y@users.noreply.github.com>
2024-10-18 22:27:29 +0000

    Added root route component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
 import { StyledEngineProvider } from "@mui/material";
+import Root from "./routes/Root";
 // Setup Tailwind CSS:
 import "./index.css";
 // CSS for Roboto which is used by MUI:
@@ -13,8 +14,15 @@ import "@fontsource/roboto/700.css";
 
 const router = createBrowserRouter([
   {
+    // This is the root layout. All pages should be a child of this
     path: "/",
-    element: <Home />,
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
