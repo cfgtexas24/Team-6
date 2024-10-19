96601d0f532dfe5b6e1c7488a6d77aa43886af3a
diauu <diana.rios4545@gmail.com>
2024-10-18 23:56:07 -0500

    added tables

--- backend/table.py
+++ backend/table.py
@@ -1,28 +1,39 @@
 from flask import Flask, jsonify
-import sqlite3
+import mysql.connector
+from config import Config
+app= Flask(name)
+
+app.config.from_object(Config)
 
 
-from flask import Flask, jsonify 
-import sqlite3 
+conn = mysql.connector.connect(
+            username=app.config['MYSQL_USER'],
+            password=app.config['MYSQL_PASSWORD'],
+            host=app.config['MYSQL_HOST'],
+            database=app.config['MYSQL_DB']
+        ) 
 
-app= Flask(__name__)
+cursor = conn.cursor()
 
 
-conn=sqlite3.connect("database.db")
 print("database open successfully!!")
-conn.execute('''
-      CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT, 
+cursor.execute('''
+      CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT,
                                       user_name TEXT NOT NULL,
                                       password TEXT NOT NULL,
+                                      role TEXT NOT NULL,
                                       email TEXT NOT NULL UNIQUE)''')
 
 
 print("table created successfully")
 
+cursor.execute('''CREATE TABLE IF NOT EXIST Employer(id INTEGER PRIMARY KEY AUTOINCREMENT,
+               email TEXT NOT NULL UNIQUE) ''' )
 
-conn.execute('''
+
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS Post(
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         content TEXT NOT NULL,
         username TEXT NOT NULL,
@@ -33,14 +44,14 @@ conn.execute('''
 ''')
 print("table post created successfully")
 
-conn.execute('''
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS comments(
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
-        post_id INTEGER NOT NULL,  
+        post_id INTEGER NOT NULL,
         username TEXT NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        user_id INTEGER, 
+        user_id INTEGER,
         FOREIGN KEY(post_id) REFERENCES Post(id),
         FOREIGN KEY(user_id) REFERENCES Users(id)
     )
@@ -49,16 +60,10 @@ print("table comments created successfully")
 
 
 
-conn=sqlite3.connect("backend/database.db")
-
-
-conn.execute('''
-      DROP TABLE Forums;
-''')
 
 
 print("table created successfully")
-conn.close()
+cusor.close()
 
 
 
