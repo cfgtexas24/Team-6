acdf4eb0bd28b086fcdf6a8b5e822837a011163d
diauu <diana.rios4545@gmail.com>
2024-10-19 00:21:32 -0500

    edit

--- backend/table.py
+++ backend/table.py
@@ -43,7 +43,7 @@ cursor.execute('''
 
 print("table created successfully")
 
-cursor.execute('''CREATE TABLE IF NOT EXIST Employer(id INT PRIMARY KEY AUTO_INCREMENT,
+cursor.execute('''CREATE TABLE IF NOT EXISTS Employer(id INT PRIMARY KEY AUTO_INCREMENT,
                email VARCHAR(50) NOT NULL UNIQUE) ''' )
 
 
