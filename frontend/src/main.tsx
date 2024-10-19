8df73b817511b39e32c9b218aba3acaff5fa727e
andr <64496558+Ayndrs@users.noreply.github.com>
2024-10-18 19:06:57 -0700

    main.tsx JobBoard fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
-import StudentProfile from "./routes/StudentProfile"
-import JobBoard from "./routes/JobBoard"
+import StudentProfile from "./routes/StudentProfile";
+import JobBoard from "./routes/JobBoard.tsx";
 import { StyledEngineProvider } from "@mui/material";
 import Root from "./routes/Root";
 // Setup Tailwind CSS:
