44ee9ea273922672852278fe206853f8499dbd21
md-y <15069105+md-y@users.noreply.github.com>
2024-10-19 02:14:06 +0000

    Finalize auth based on API

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,8 +1,70 @@
-import { Typography } from "@mui/material";
-import { FC } from "react";
+import {
+  Alert,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { FC, FormEventHandler, useState } from "react";
+import { login } from "../util/authentication";
+import { useNavigate } from "react-router-dom";
 
 const Login: FC = () => {
-  return <Typography variant="h1">This is the login page</Typography>;
+  const navigate = useNavigate();
+  const [username, setUsername] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const { refetch: loginFetch, error } = useQuery({
+    queryKey: ["login", { username, password }] as const,
+    queryFn: ({ queryKey }) =>
+      login(queryKey[1].username!, queryKey[1].password!),
+    enabled: false, // Only fetch when the button is pressed
+    retry: false,
+  });
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    loginFetch().then((res) => {
+      if (res.isError) return;
+      const url = new URL(window.location.href);
+      const redirectParam = url.searchParams.get("redirect");
+      if (redirectParam) {
+        navigate(redirectParam);
+      } else {
+        navigate("/");
+      }
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Container maxWidth="md">
+          <Stack gap="1rem">
+            <Typography variant="h2">Login</Typography>
+            <TextField
+              label="Username"
+              type="text"
+              name="username"
+              required
+              onChange={(ev) => setUsername(ev.target.value)}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              name="password"
+              required
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
+            <Button type="submit">Login</Button>
+            {error && <Alert severity="error">{error.message}</Alert>}
+          </Stack>
+        </Container>
+      </form>
+    </>
+  );
 };
 
 export default Login;
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,5 +1,4 @@
-import { Alert, LinearProgress } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
+import { Alert } from "@mui/material";
 import { FC, useEffect } from "react";
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import { checkUserAuth } from "../util/authentication";
@@ -15,37 +14,32 @@ const Root: FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const { data: isAuthenticated, isLoading } = useQuery({
-    queryKey: ["checkUserAuth"],
-    staleTime: 60 * 1000 * 60, // Only check auth every hour (unless invalidated by login/logout)
-    queryFn: checkUserAuth,
-  });
+  const isAuthenticated = checkUserAuth();
+  console.log(isAuthenticated);
 
   const isAuthenticatedRoute = unauthenticatedPaths.every(
     (path) => path !== location.pathname,
   );
 
   useEffect(() => {
-    if (!isLoading && !isAuthenticated && isAuthenticatedRoute) {
-      // Go to login page if trying to access authenticated route while unauthenticated
-      navigate("/login", { replace: true });
-    }
-  }, [isLoading, isAuthenticated, isAuthenticatedRoute, navigate]);
-
-  if (isAuthenticatedRoute) {
-    if (isLoading) {
-      return <LinearProgress />;
-    }
-
-    if (!isAuthenticated) {
-      // This component should not be displayed since the redirect should be automatic.
-      // However, just in case, this gives the login link too
-      return (
-        <Alert severity="error">
-          You are not logged in. Please login <Link to="/login">here</Link>
-        </Alert>
+    // Go to login page if trying to access authenticated route while unauthenticated
+    if (!isAuthenticated && isAuthenticatedRoute) {
+      // Add redirect path for after login
+      const redirectUrl = encodeURIComponent(
+        window.location.pathname + window.location.search,
       );
+      navigate(`/login?redirect=${redirectUrl}`, { replace: true });
     }
+  }, [isAuthenticated, isAuthenticatedRoute, navigate]);
+
+  if (isAuthenticatedRoute && !isAuthenticated) {
+    // This component should not be displayed since the redirect should be automatic.
+    // However, just in case, this gives the login link too
+    return (
+      <Alert severity="error">
+        You are not logged in. Please login <Link to="/login">here</Link>
+      </Alert>
+    );
   }
 
   // TODO: Add navbar here
