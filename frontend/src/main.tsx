1e14206d9df4d3a98980259af501f192f34a604b
md-y <15069105+md-y@users.noreply.github.com>
2024-10-19 03:37:42 +0000

    Create basic forum page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
-import StudentProfile from "./routes/StudentProfile"
+import StudentProfile from "./routes/StudentProfile";
 import { StyledEngineProvider } from "@mui/material";
 import Root from "./routes/Root";
 // Setup Tailwind CSS:
@@ -12,6 +12,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import Forum from "./routes/Forum";
 
 const router = createBrowserRouter([
   {
@@ -23,12 +24,24 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: "/community-forums",
+        element: <Forum />,
+      },
+      {
+        path: "/community-forums/:category",
+        element: <Forum />,
+      },
+      {
+        path: "/community-forums/:category/:post",
+        element: <Forum />,
+      },
     ],
   },
   {
     path: "/student-profile",
     element: <StudentProfile />,
-  }
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
