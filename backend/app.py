1df6c12c5069f63fcee148b572bace4beaa3879b
Diana <diana.rios4545@gmail.com>
2024-10-18 19:15:06 -0500

    added employer to app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,10 @@ from flask import Flask, jsonify,request
 from table import conn
 import sqlite3 
 
+
+
 app= Flask(__name__)
+
 
 def get_conn_database():
     conn=sqlite3.connect('database.db')
@@ -34,8 +37,41 @@ def add_user():
         return(jsonify({'message':'you must include a username, password and an email'}),400)
 
     conn=get_conn_database()
-    conn.execute('INSERT INTO Users(first_name, last_name, email) VALUE(?,?,?)',(user_name,password,email))
+    conn.execute('INSERT INTO Users (user_name, password, email) VALUES (?, ?, ?)', (user_name, password, email))
     conn.commit()
     conn.close()
 
     return jsonify({'message':'User added sucessfully!'}),201
+
+
+#route to get all Employers
+@app.route('/Employers',methods=['GET'])
+def get_employers():
+    conn=get_conn_database()
+    Employers=conn.execute('SELECT * FROM Employers').fetchall()
+    conn.close()
+
+    #convert data fetch into dictionary and send json reponse 
+
+    user_list=[dict(Employers) for user in Employers]
+    return jsonify(user_list)
+
+
+#route to add new user
+@app.route('/Employers', methods=['POST'])
+def add_user():
+    data=request.get_json()
+    email=data['email']
+
+    if not email:
+        return(jsonify({'message':'Need to provide email'}),400)
+
+    conn=get_conn_database()
+    conn.execute('INSERT INTO Employers (email) VALUES (?)', ( email))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message':'User added sucessfully!'}),201
+
+if __name__ == '__main__':
+    app.run(debug=True)
