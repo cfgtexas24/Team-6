0fce2f6b4233ef535f413b109dcf53b4143ef192
Nafi Islam <nafi.islam321@gmail.com>
2024-10-18 19:26:33 -0500

    LandingNavBar complete

---
+++ frontend/src/components/LandingNavBar.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { AppBar, Toolbar, Tabs, Tab, Button, Box } from "@mui/material";
+import { Link as ScrollLink } from "react-scroll";
+import { Link } from "react-router-dom"; // Assuming you're using react-router-dom for navigation
+import REE from "../assets/REE.png";
+
+const LandingNavbar: React.FC = () => {
+  return (
+    <AppBar position="sticky" color="transparent" elevation={0}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        {/* Logo */}
+        <Box component="div" sx={{ display: "flex", alignItems: "center" }}>
+          <img src={REE} alt="Logo" style={{ height: "50px" }} />
+        </Box>
+
+        {/* Navigation Tabs */}
+        <Tabs>
+          <Tab
+            label="About"
+            component={ScrollLink}
+            to="homeSection"
+            smooth={true}
+            duration={500}
+            spy={true}
+            offset={-70}
+          />
+          <Tab
+            label="Education"
+            component={ScrollLink}
+            to="educationSection"
+            smooth={true}
+            duration={500}
+            spy={true}
+            offset={-70}
+          />
+          <Tab
+            label="Careers"
+            component={ScrollLink}
+            to="careersSection"
+            smooth={true}
+            duration={500}
+            spy={true}
+            offset={-70}
+          />
+        </Tabs>
+
+        {/* Login and Sign Up Buttons */}
+        <Box>
+          <Button
+            component={Link}
+            to="/login"
+            variant="outlined"
+            sx={{
+              color: "#FEC10E", // REE Yellow
+              borderColor: "#FEC10E", // Outline color
+              "&:hover": {
+                borderColor: "#E0A90C", // Darker outline on hover
+                color: "#E0A90C", // Matching text color on hover
+              },
+              marginRight: "1rem",
+            }}
+          >
+            Log In
+          </Button>
+
+          <Button
+            component={Link}
+            to="/signup"
+            variant="contained"
+            sx={{
+              backgroundColor: "#FEC10E", // REE Yellow
+              color: "#fff", // White, text color
+              "&:hover": {
+                backgroundColor: "#E0A90C", // Darker yellow on hover
+              },
+            }}
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default LandingNavbar;
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,14 +1,21 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
+import LandingNavBar from "../components/LandingNavBar";
 
 /**
  * This component is present on every route.
  * The page content is rendered where <Outlet/> is
  */
+
 const Root: FC = () => {
   // TODO: Add entrypoint auth logic here
-  // TODO: Add navbar here
-  return <Outlet />;
+  // Fix: Might be easier to not use NavBar in root? Will need unique NavBars for each page i.e. LandingNavBar, UserNavBar, EmployerNavBar, etc.
+  return (
+    <>
+      <LandingNavBar />
+      <Outlet />
+    </>
+  );
 };
 
 export default Root;
