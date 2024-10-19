155c251ce22a8bd9fb515378eca8a261dc11a00c
smanwani22 <serenamanwani09@gmail.com>
2024-10-18 23:04:50 -0500

    Added employer profile to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,6 +11,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import EmployerProfilePage from "./routes/EmployerProfile";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/employer-profile",
+        element: <EmployerProfilePage />,
       },
     ],
   },
