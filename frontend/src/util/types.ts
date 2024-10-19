cba1957be0e9d522a54af0ebf9c3c8d86309f270
md-y <15069105+md-y@users.noreply.github.com>
2024-10-19 10:12:29 +0000

    Add new post button and form

---
+++ frontend/src/util/types.ts
@@ -0,0 +1,7 @@
+export enum ForumCategory {
+  SupportGroup = "Support Group",
+  Course = "Courses",
+  Disability = "Disability Assistance",
+  Housing = "Housing Assistance",
+  General = "General",
+}
